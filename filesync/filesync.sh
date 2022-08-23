4edbc04b50aeb0bd77c8d99896be403d6269c48b
strickland.rbls <strickland.rbls@gmail.com>
2022-08-22 22:20:17 -0500

    Reads from mediabackup.conf now

---
+++ filesync/filesync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+case "$1" in
+    setup)
+        echo "This is the setup statement"
+        ;;
+    sync)
+        echo "This is the sync statement"
+        ;;
+esac
--- media-backup/mediabackup.sh
+++ media-backup/mediabackup.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
+#CONFDIR=/etc/mediabackup/
+CONFDIR=./
+HOLDINGDIR=/tmp/
+
+if ! [ -e "${CONFDIR}"mediabackup.conf ]; then
+   echo "No configuration file can be found at "${CONFDIR:-1}
+   exit 1
+fi
+
+BACKUPS=$(awk -F ':' '{if($1 == "backup"){print $2":"$3":"$4}}' ${CONFDIR}/mediabackup.conf)
+for item in $BACKUPS; do
+    file=`echo ${item} | awk -F ':' '{print $1}'`
+    if ! [ -e ${file} ]; then
+        echo "${file} does not exist"
+    else
+        tarfile=`echo ${item} | awk -F ':' '{print $2}'`
+        dest=`echo ${item} | awk -F ':' '{print $3}'`
+        if [ -e ${HOLDINGDIR}${tarfile::-3} ]; then
+            tar --append -f ${HOLDINGDIR}${tarfile::-3} ${file}
+            echo "Appending ${file} to ${tarfile::-3}"
+        else
+            tar -cf ${HOLDINGDIR}${tarfile::-3} ${file}
+            echo "Creating ${file} to ${tarfile::-3}"
+        fi
+    fi
+
+    if [ -e ${dest} ]; then
+        mv ${HOLDINGDIR}${tarfile::-3} ${dest}
+    else
+        echo "Destination ${dest} does not exist. Archives will sit in /tmp"
+    fi
+done
+
 TIME=$(date | awk '{print $2,$3,$4}')
-tar -cf /mnt/backups/books.tar.xz -C /mnt/NAS --xz Books
-tar -cf /mnt/backups/docs.tar.xz -C /mnt/NAS --xz Documents
-tar -cf /mnt/backups/photos.tar.xz -C /mnt/NAS --xz Photos
 echo "Completed backup at: "${TIME}
